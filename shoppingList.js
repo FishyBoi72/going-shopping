6a440bc7da35c559eeedfb7364f439dfdcac08fb
FishFlete <fishflete@gmail.com>
2024-06-11 14:21:21 -0400

    changed chips and salsa to rice and beans

--- shoppingList.js
+++ shoppingList.js
@@ -3,4 +3,9 @@ shoppingList.push('fruit loops');
 let coffeeIndex = shoppingList.indexOf('coffee');
 if (coffeeIndex !== -1) {
     shoppingList[coffeeIndex] = 'fair trade coffee';
-    
+}
+let chipsIndex = shoppingList.indexOf('chips');
+let salsaIndex = shoppingList.indexOf('salsa');
+if (chipsIndex !== -1 && salsaIndex !== -1 && salsaIndex === chipsIndex + 1) {
+    shoppingList.splice(chipsIndex, 2, 'rice', 'beans');
+}
