4174fb1e0568f447341cb3d19b06ca44b5a8e855
FishFlete <fishflete@gmail.com>
2024-06-11 14:16:10 -0400

    changed coffee to fair trade coffee

--- shoppingList.js
+++ shoppingList.js
@@ -1,2 +1,6 @@
 let shoppingList = ['pop tarts', 'ramen noodles', 'chips', 'salsa', 'coffee']
 shoppingList.push('fruit loops');
+let coffeeIndex = shoppingList.indexOf('coffee');
+if (coffeeIndex !== -1) {
+    shoppingList[coffeeIndex] = 'fair trade coffee';
+    
